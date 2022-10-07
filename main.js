c005c2ff1214a9b4cdfb9476b7e7442193d8a37f
kaled molina <kaled.molina@campusucc.edu.co>
2022-10-07 10:09:01 -0500

    falta botones listo solo falta sutom y reset

--- main.js
+++ main.js
@@ -1,4 +1,50 @@
 const billInput = document.querySelector('.bill-input');
 const peopleInput = document.querySelector('.people-input');
-const tipPerson = document.querySelector('.tip-amount');
-const totalPerson = document.querySelector('.total-input');
+const tipPerPerson = document.getElementById('tip-amount');
+const totalPerPerson = document.getElementById('total-amount');
+const tips = document.querySelectorAll(".tips");
+
+billInput.addEventListener('input', billInputFun);
+peopleInput.addEventListener('input', peopleInputFun);
+tips.forEach(function(val){
+    val.addEventListener('click', handleClick);
+});
+
+
+billInput.value ="0.0";
+peopleInput.value = "1";
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2); 
+totalPerPerson.innerHTML = "$" + (0.0).toFixed(2); 
+
+let billValue = 0.0;
+let peopleValue = 1;
+let tipValue = 0.15;
+
+function billInputFun (){
+    billValue = parseFloat(billInput.value)
+    calculateTip()
+};
+function peopleInputFun (){
+    peopleValue = parseFloat(peopleInput.value)
+    calculateTip()
+};
+function handleClick(event){
+tips.forEach(function(val){
+    val.classList.remove("active-tip");
+if (event.target.innerHTML == val.innerHTML){
+    val.classList.add("active-tip");
+    tipValue =parseFloat(val.innerHTML)/100
+}
+});
+calculateTip()
+};
+
+function calculateTip(){
+    if (peopleValue >= 1) {
+        let tipAmount = (billValue * tipValue) / peopleValue;
+        let total = (billValue * tipAmount) / peopleValue;
+        tipPerPerson.innerHTML = "$" + tipAmount.toFixed(2); 
+        totalPerPerson.innerHTML = "$" + total.toFixed(2);
+    };
+};
+
