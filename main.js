64a77ab6221743581f460e8cda30ece851e250a1
kaled molina <kaled.molina@campusucc.edu.co>
2022-10-07 10:55:48 -0500

    final

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ const peopleInput = document.querySelector('.people-input');
 const tipPerPerson = document.getElementById('tip-amount');
 const totalPerPerson = document.getElementById('total-amount');
 const tips = document.querySelectorAll(".tips");
+const tipCustom = document.querySelector(".tip-custom");
+const resetBtn = document.querySelector(".reset");
+const error = document.querySelector(".error");
 
 billInput.addEventListener('input', billInputFun);
 peopleInput.addEventListener('input', peopleInputFun);
 tips.forEach(function(val){
     val.addEventListener('click', handleClick);
 });
+tipCustom.addEventListener('input',tipInputFun); 
+resetBtn.addEventListener ('click', reset);
 
 
 billInput.value ="0.0";
@@ -21,13 +26,34 @@ let peopleValue = 1;
 let tipValue = 0.15;
 
 function billInputFun (){
-    billValue = parseFloat(billInput.value)
-    calculateTip()
+    billValue = parseFloat(billInput.value);
+    calculateTip();
 };
+
 function peopleInputFun (){
-    peopleValue = parseFloat(peopleInput.value)
-    calculateTip()
+    peopleValue = parseFloat(peopleInput.value);    
+
+    if(peopleValue < 1 ){
+        error.style.display = 'flex';
+        peopleInput.style.border = 'thick solid red';
+    }else{
+        error.style.display = 'none';
+        peopleInput.style.border = 'none';
+        calculateTip();
+    };
+    
 };
+
+
+function tipInputFun (){
+    tipValue = parseFloat(tipCustom.value / 100);
+
+    tips.forEach(function(val){
+        val.classList.remove("active-tip");
+    });
+    calculateTip();
+};
+
 function handleClick(event){
 tips.forEach(function(val){
     val.classList.remove("active-tip");
@@ -36,7 +62,7 @@ if (event.target.innerHTML == val.innerHTML){
     tipValue =parseFloat(val.innerHTML)/100
 }
 });
-calculateTip()
+calculateTip();
 };
 
 function calculateTip(){
@@ -48,3 +74,10 @@ function calculateTip(){
     };
 };
 
+function reset(){
+    billInput.value = "0.0";
+    billInputFun();
+    peopleInput.value = "1";
+    peopleInputFun();
+    tipCustom.value = "";
+}
